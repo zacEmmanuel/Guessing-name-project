ecdaef2f35d023f5470d4118a8c6fb53159e023a
Isaac Emmanuel <isaacemm740@gmail.com>
2025-11-17 22:56:41 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
-        // NUMBER GUESSING GAME
-
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
